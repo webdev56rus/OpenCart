628df26373aeade560a6c74fb72174860ea5d14a
webdev56rus <webdev56rus@mail.ru>
2016-05-12 20:15:36 +0500

    First Commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.domain/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.domain/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Sergey/Documents/sites/OpenCart/catalog/');
+define('DIR_SYSTEM', '/Users/Sergey/Documents/sites/OpenCart/system/');
+define('DIR_DATABASE', '/Users/Sergey/Documents/sites/OpenCart/system/database/');
+define('DIR_LANGUAGE', '/Users/Sergey/Documents/sites/OpenCart/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Sergey/Documents/sites/OpenCart/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Sergey/Documents/sites/OpenCart/system/config/');
+define('DIR_IMAGE', '/Users/Sergey/Documents/sites/OpenCart/image/');
+define('DIR_CACHE', '/Users/Sergey/Documents/sites/OpenCart/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Sergey/Documents/sites/OpenCart/download/');
+define('DIR_LOGS', '/Users/Sergey/Documents/sites/OpenCart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'OpenCart');
+define('DB_PREFIX', 'oc_');
+?>
